2e7c704741ae13deefd310e39ecb9325a580391e
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-08-03 08:39:43 +0300

    task no.14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_balance - Checks to see if the binary tree is balanced
+  * @tree: tree to measure
+  * Return: left - right.
+  */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (!tree)
+		return (0);
+	left = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	right = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	return (left - right);
+}
